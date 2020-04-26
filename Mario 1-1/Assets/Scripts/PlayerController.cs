eb78a404ae84a0b45b3158c2d130db219679140d
Jonathan <jonathan.gunnells@yahoo.com>
2020-04-26 15:43:07 -0500

    First Pass
    Finished setting up colliders for player, ground, and obstacles. Added movement and broken jump for player.

---
+++ Mario 1-1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    public float speed, height = 2.0f;
+
+    void Start()
+    {
+        speed = 100;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, rb.velocity.y);
+        if(Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W)) || Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, height);
+        }
+    }
+}
